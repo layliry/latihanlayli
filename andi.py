98d4d4b5d37250efdda4dfd6b9fa65c851d18ec2
Layli Rahmania Yuwono <layliry@gmail.com>
2022-06-06 17:19:20 +0700

    edit 1

--- andi.py
+++ andi.py
@@ -1,3 +1,10 @@
+nama = "layli rahmania yuwono"
+hobi = ["makan" , "tidur", "nonton"]
+sekolah = {
+    "SMA" : "Al Irsyad",
+    "Uni" : "ITB"
+}
+
 def panggil(tempat):
 
     return "selamat datang di" + tempat
