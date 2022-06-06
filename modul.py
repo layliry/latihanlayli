3f30bf14d6ce302e163c06a611c2059d4e80bef9
Layli Rahmania Yuwono <layliry@gmail.com>
2022-06-06 16:26:32 +0700

    modul

---
+++ modul.py
@@ -0,0 +1,18 @@
+namalengkap = "Layli Rahmania"
+hobi = ["makan", "tidur", "mengeluh"]
+Sekolah = {
+    "SD":"SD IT 01",
+    "SMP":"SMP Al Irsyad",
+    "SMA": "SMA IT",
+    "PT" : "ITB",
+}
+
+def panggil(tempat, waktu = "siang", *nama):
+
+    print("selamat datang di", tempat)
+
+    for n in nama :
+        print ("halo", n, "selamat", waktu)
+
+class array :
+    pass
