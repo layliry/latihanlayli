9e2433e20edbac8d589a4f7363939170090241b3
Layli Rahmania Yuwono <layliry@gmail.com>
2022-06-13 11:11:43 +0700

    .

---
+++ Package/persegi.py
@@ -0,0 +1,9 @@
+bentuk = "persegi panjang"
+variabel = ["p","l"]
+nilai = {
+    "p" : "panjang",
+    "l" : "lebar"
+}
+
+def persegi(p,l) :
+    print(p*l)
---
+++ Package/segitiga.py
@@ -0,0 +1,9 @@
+bentuk = "Segitiga"
+variabel = ["a","t"]
+nilai = {
+    "a" : "alas",
+    "t" : "tinggi"
+}
+
+def segitiga(a,t) :
+    print(a*t/2)
